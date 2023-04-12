22f3344053d6724df6b250f400aa3246cd588cf5
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-04-12 17:58:15 -0400

    edit variables

--- main.tf
+++ main.tf
@@ -4,18 +4,20 @@ data "aws_availability_zones" "available" {}
 
 locals {
   app_name = var.app_name
-  region   = "us-east-1"
-  vpc_cidr = "10.0.0.0/16"
+  region   = var.region
+  vpc_cidr = var.vpc_cidr
   azs      = slice(data.aws_availability_zones.available.names, 0, 2)
 }
 
 module "vpc" {
   source                  = "terraform-aws-modules/vpc/aws"
+  version                 = "4.0.1"
   name                    = local.app_name
   cidr                    = local.vpc_cidr
-  enable_dns_hostnames    = true
-  map_public_ip_on_launch = true
+  enable_dns_hostnames    = var.enable_dns_hostnames
+  map_public_ip_on_launch = var.map_public_ip_on_launch
 
+  #subnets
   azs             = local.azs
   public_subnets  = [for key, value in local.azs : cidrsubnet(local.vpc_cidr, 8, key + 100)]
   private_subnets = [for key, value in local.azs : cidrsubnet(local.vpc_cidr, 8, key + 4)]
@@ -24,15 +26,10 @@ module "vpc" {
   # database_subnet_names = [for name in var.database_subnet_names : name]
 
 
-  enable_nat_gateway = true
+  enable_nat_gateway = var.enable_nat_gateway
 
   manage_default_route_table    = false
   manage_default_security_group = false
   manage_default_network_acl    = false
 
-}
-
-resource "aws_main_route_table_association" "public" {
-  vpc_id         = module.vpc.vpc_id
-  route_table_id = module.vpc.public_route_table_ids[0]
 }
