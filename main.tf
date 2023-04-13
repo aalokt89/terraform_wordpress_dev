6f97c9e6094bf06c7ff8a9f9280f02a87cb16513
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-04-12 21:17:14 -0400

    fix private subnets

--- main.tf
+++ main.tf
@@ -3,10 +3,12 @@
 data "aws_availability_zones" "available" {}
 
 locals {
-  app_name = var.app_name
-  region   = var.region
-  vpc_cidr = var.vpc_cidr
-  azs      = slice(data.aws_availability_zones.available.names, 0, 2)
+  app_name    = var.app_name
+  environment = var.environment
+  region      = var.aws_region
+  vpc_cidr    = var.vpc_cidr
+  azs         = slice(data.aws_availability_zones.available.names, 0, 2)
+
 }
 
 module "vpc" {
@@ -14,22 +16,22 @@ module "vpc" {
   version                 = "4.0.1"
   name                    = local.app_name
   cidr                    = local.vpc_cidr
-  enable_dns_hostnames    = var.enable_dns_hostnames
+  enable_dns_hostnames    = var.vpc_enable_dns_hostnames
   map_public_ip_on_launch = var.map_public_ip_on_launch
 
-  #subnets
-  azs             = local.azs
+  azs = local.azs
+
+  # publiic subnets
   public_subnets  = [for key, value in local.azs : cidrsubnet(local.vpc_cidr, 8, key + 100)]
   private_subnets = [for key, value in local.azs : cidrsubnet(local.vpc_cidr, 8, key + 4)]
 
-  # public_subnet_names   = [for name in var.public_subnet_names : name]
-  # database_subnet_names = [for name in var.database_subnet_names : name]
-
-
   enable_nat_gateway = var.enable_nat_gateway
+  single_nat_gateway = true
 
-  manage_default_route_table    = false
-  manage_default_security_group = false
-  manage_default_network_acl    = false
+
+
+
+
+
 
 }
