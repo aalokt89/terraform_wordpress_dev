7fce0773db36b589355c7aa7fe10d137504213b9
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-04-21 20:40:40 -0400

    fix useer_data

--- auto_scaling.tf
+++ auto_scaling.tf
@@ -33,41 +33,7 @@ module "wordpress_asg" {
   instance_type     = var.wordpress_template_instance_type
   enable_monitoring = var.wordpress_template_enable_monitoring
   security_groups   = [aws_security_group.ssh_sg.id, aws_security_group.wordpress_ec2_sg.id]
-  user_data         = <<-EOF
-            #!/bin/bash
-            sudo yum update -y
-            sudo yum install -y httpd mysql
-            sudo systemctl start httpd
-            sudo systemctl enable httpd
-
-            sudo amazon-linux-extras install -y php7.4
-            sudo amazon-linux-extras enable php7.4
-            sudo yum clean metadata
-            sudo yum install php-cli php-pdo php-fpm php-json php-mysqlnd
-
-            #Change OWNER and permission of directory /var/www
-            sudo usermod -a -G apache ec2-user
-            sudo chown -R ec2-user:apache /var/www
-            sudo find /var/www -type d -exec chmod 2775 {} \;
-            sudo find /var/www -type f -exec chmod 0664 {} \;
-
-            # Download and install the WordPress CLI
-            sudo curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-            sudo chmod +x wp-cli.phar
-            sudo mv wp-cli.phar /usr/local/bin/wp
-
-            # Create the WordPress configuration file
-            cd /var/www/html
-            wp core download --allow-root
-            wp config create --dbname=wordpress --dbuser=admin --dbpass=password --dbhost=wordpress.cgwzuzkgdaxy.us-east-1.rds.amazonaws.com --allow-root
-
-            # Set the appropriate permissions
-            sudo chown -R apache:apache /var/www/html
-            sudo chmod -R 755 /var/www/html
-            sudo systemctl restart httpd.service
-            EOF
-
-
+  user_data         = filebase64("install_wordpress.sh")
 
   scaling_policies = {
     avg-cpu-policy-greater-than-75 = {
