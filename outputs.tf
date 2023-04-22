53984c48c84e091e15d01b267d3aec864c614e27
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-04-22 15:40:20 -0400

    output public IPs

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,12 @@ output "alb_dns" {
   description = "ALB public DNS"
   value       = "http://${aws_lb.web_alb.dns_name}"
 }
+
+data "aws_instances" "wordpress_instances" {
+  instance_state_names = "running"
+}
+output "instance_public_ips" {
+  description = "get public IPs of instances"
+  value       = data.aws_instances.wordpress_instances.public_ips
+}
+
